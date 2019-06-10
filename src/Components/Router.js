0fbd3821ca8a2f45e2ad0bfb4eed9bcc7f0d0aa6
Jerome <jerome@spinprotocol.io>
2019-06-10 23:49:27 +0900

    Add Styled-Components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -6,6 +6,7 @@ import {
   Switch,
 } from 'react-router-dom';
 
+import Header from '../Components/Header';
 import Home from '../Routes/Home';
 import TV from '../Routes/TV';
 import Search from '../Routes/Search';
@@ -13,12 +14,15 @@ import Search from '../Routes/Search';
 export default () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/tv" exact component={TV} />
-        <Route path="/search" component={Search} />
-        <Redirect from="*" to="/" />
-      </Switch>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/tv" exact component={TV} />
+          <Route path="/search" component={Search} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </React.Fragment>
     </Router>
   );
 };
