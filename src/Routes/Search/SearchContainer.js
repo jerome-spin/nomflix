81809f36e0fd2f78083257c2a4dcd7a22a5f7481
Jerome <jerome@spinprotocol.io>
2019-06-16 19:26:51 +0900

    Fetch the data in TVContainer & Add Detail in Router

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { moviesApi, tvApi } from '../../api';
 
 export default class extends React.Component {
   state = {
     movieResults: null,
     tvResults: null,
     searchTerm: '',
-    loading: true,
+    loading: false,
     error: null,
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== '') {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+
+      this.setState({ movieResults, tvResults });
+    } catch (error) {
+      this.setState({ error: 'Can not find results.' });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -20,6 +49,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
