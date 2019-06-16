64a691d07de257c562bba9458889a58787fc726b
Jerome <jerome@spinprotocol.io>
2019-06-17 08:32:52 +0900

    Fetch the data in Detail Container

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from '../../api';
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes('/movie/'),
+    };
+  }
+
+  componentDidMount = async () => {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push('/');
+    }
+
+    const { isMovie } = this.state;
+    let result = null;
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+    } catch (error) {
+      this.setState({ error: 'Can not find anything ' });
+    } finally {
+      this.setState({ loading: false, result });
+    }
   };
 
   render() {
